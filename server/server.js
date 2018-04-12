328e26be530c08d2da7828c1cc620ef0580afb95
DavidWei95036593 <dw36593@pausd.us>
2018-04-12 10:28:00 -0700

    this should fix it

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-const app = express();
+
 const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
 // const {Dog}=require(`./models/dog.js`);
@@ -9,7 +9,19 @@ app.use(bodyParser.json());
 app.get(`/`,(req,res)=>{
   res.send("It worksa");
 })
-
+app.post(`/dogs`,(req,res)=>{
+  const dog = new Dog({
+    name:`Spot`,
+    age:2
+  })
+  dog.save()
+    .then(dog=>{
+      res.send(dog);
+    })
+    .catch(error=>{
+      res.status(400).send();
+    })
+})
 
 app.listen(port,()=>{
   console.log(`listen to port ${port}`);
