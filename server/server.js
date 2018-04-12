d1a3a5d82222f1b0f271dad5441b6ef1bdb6c8c8
DavidWei95036593 <dw36593@pausd.us>
2018-04-12 10:26:58 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require(`express`);
 const app = express();
+const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
+// const {Dog}=require(`./models/dog.js`);
+const app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 app.get(`/`,(req,res)=>{
   res.send("It worksa");
 })
