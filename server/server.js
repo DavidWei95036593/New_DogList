1ba3ea232ea0a8b42e1b00e0314e8d929c3eede2
DavidWei95036593 <dw36593@pausd.us>
2018-04-12 10:31:34 -0700

    add mongoose promise

--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ const mongoose = require(`mongoose`);
 // const {Dog}=require(`./models/dog.js`);
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise;
+const database = process.env.MONGODB_URI||`mogodb://localhost:27017`;
+mongoose.connect(database)
+.then(()=>{
+  console.log(`connected to database`);
+}).catch((error)=>{
+  console.log(error);
+  console.log(`unable to connect to database`);
+})
+
+
 app.use(bodyParser.json());
 app.get(`/`,(req,res)=>{
   res.send("It works");
