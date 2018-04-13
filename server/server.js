cd7ce914c737ff06cf6f0d8e8d16249e0763156e
DavidWei95036593 <dw36593@pausd.us>
2018-04-13 12:31:52 -0700

    add dog

--- server/server.js
+++ server/server.js
@@ -1,40 +1,41 @@
-const express = require(`express`);
-
-const bodyParser = require(`body-parser`);
-const mongoose = require(`mongoose`);
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const {Dog} = require('./models/dog.js')
+
 const app = express();
+
+//setup
 const port = process.env.PORT || 3000;
-
 mongoose.Promise = global.Promise;
-const database = process.env.MONGODB_URI||`mogodb://localhost:27017`;
+const database = process.env.MONGODB_URI || "mongodb://localhost:27017/Doglist"
 mongoose.connect(database)
-.then(()=>{
-  console.log(`connected to database`);
-}).catch((error)=>{
-  console.log(error);
-  console.log(`unable to connect to database`);
-})
-
+  .then(() => {
+    console.log('connected to database');
+  }).catch(() => {
+    console.log('unable to connect to database');
+  })
 
 app.use(bodyParser.json());
-app.get(`/`,(req,res)=>{
-  res.send("It works");
+
+app.get('/', (req, res) => {
+  res.send("GET /");
 })
-app.post(`/dogs`,(req,res)=>{
+
+app.post('/dogs', (req, res) => {
   const dog = new Dog({
-    name:`Spot`,
-    age:2
+    name: 'Spot',
+    age: 2
   })
   dog.save()
-    .then(dog=>{
+    .then(dog => {
       res.send(dog);
     })
-    .catch(error=>{
+    .catch(e => {
       res.status(400).send();
     })
 })
 
-app.listen(port,()=>{
-  console.log(`listen to port ${port}`);
+app.listen(port, () => {
+  console.log( `Listening on port ${port}`);
 })
