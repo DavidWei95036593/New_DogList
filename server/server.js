399348683e2f2840352befdf05bb68ac993f09ad
DavidWei95036593 <dw36593@pausd.us>
2018-04-13 12:24:19 -0700

    add dog

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 
 const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
-// const {Dog}=require(`./models/dog.js`);
+const {Dog}=require(`./models/dog.js`);
 const app = express();
 const port = process.env.PORT || 3000;
 
