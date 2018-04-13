39af4544db7525617f56a20201f1458b1302b554
DavidWei95036593 <dw36593@pausd.us>
2018-04-13 13:40:45 -0700

    testing god

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const {Dog} = require('./models/dog.js')
-
+const {Dog} = require('./models/dog.js');
+const hbs = require(`hbs`);
+const path = require(`path`);
+const _ = require(`lodash`);
 const app = express();
-
+//hbs set up
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'../app/views'));
+hbs.registerPartials(path.join(path.join(__dirname,'../app/views/partials')));
 //setup
 const port = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
@@ -17,23 +22,42 @@ mongoose.connect(database)
   })
 
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', (req, res) => {
+  res.redirect(`/dogs/new`)
   res.send("GET /");
 })
 
+app.get(`/dogs`,(req,res)=>{
+  Dog.find()
+  .then(dogs=>{
+    res.send(dogs);
+  })
+  .catch(e=>{
+    res.status(404).send();
+  })
+})
+
+app.get(`/dogs/new`,(req,res)=>{
+  res.render(`./dogs/new`);
+})
+
 app.post('/dogs', (req, res) => {
+  if(!req.body.name||!req.body.age){
+    console.log("bad request");
+    res.status(400).send();
+  }else{
   const dog = new Dog({
-    name: 'Spot',
-    age: 2
+    name: req.body.name,
+    age: req.body.age
   })
   dog.save()
     .then(dog => {
-      res.send(dog);
+      res.redirect('/dogs');
     })
     .catch(e => {
       res.status(400).send();
-    })
+    })}
 })
 
 app.listen(port, () => {
